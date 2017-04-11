74845bc57a8e286c1bbf889d84b785b534c73ad9
Hack Reactor Student <student@hackreactor.com>
2017-04-10 20:08:11 -0700

    Complete additional SongQueue functionalities
    Work in progress...

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,23 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.shift()
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    });
+
+  },
+
+  playFirst: function() {
+    
   }
 
 });
