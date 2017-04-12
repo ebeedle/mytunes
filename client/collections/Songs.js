b1226199446125fa17e41c290efda0e2be1d2fa3
Hack Reactor Student <student@hackreactor.com>
2017-04-11 19:56:07 -0700

    Finished myTunes

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,28 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
 
-});
+  url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
+  
+  parse: function(response) {
+  	return response.results;
+  },
+
+  initialize: function() {
+  	this.fetch({
+  		data: {
+  			limit:30
+  		}
+  	})
+  },
+  	
+/*
+  	for (var i = response.results.length - 1; i >= 0; i--) {
+  		results.push(response.results[i]);
+  	}
+  	console.log(results)
+  	return results;
+ */
+  });
+
